7d7608501e77b0d0290dcb322f55319160cd6906
Bose Falk <falk@dkms.de>
2018-11-20 11:35:02 +0100

    Added ?CTUtools help page listing main useful functions

---
+++ R/pkgname.R
@@ -0,0 +1,15 @@
+#' @section Main functions:
+#'
+#' \code{\link{shorten_allele}}: Reduce a long allele string (i.e. "01:02:01:03g") down to fewer fields ("01:02")
+#'
+#' \code{\link{HLA_C_classification}}: Classify two HLA-C alleles into a C1/C2 group
+#'
+#' @section Datasets:
+#'
+#' \code{\link{HLA_C_class_data}}: Reference data for C1 / C2 classification
+#'
+#' \code{\link{NMDP}}: Reference data for expanding NMDP codes
+#'
+#' @keywords internal
+"_PACKAGE"
+#> [1] "_PACKAGE"

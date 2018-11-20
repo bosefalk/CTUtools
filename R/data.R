b188c14b5e0e57865c6ddeed895e0d7b2440943e
Bose Falk <falk@dkms.de>
2018-11-20 15:43:27 +0100

    Overall HLA-B classification into Bw6/Bw4-80I/T, together with reference data

--- R/data.R
+++ R/data.R
@@ -9,9 +9,25 @@
 #'   \item{Predicted Ligand}{Predicted Ligand, "C1" or "C2"}
 #' }
 #'
-#' @seealso \code{\link{HLA_C_class_load}}
+#' @seealso \code{\link{HLA_C_class_load}}, \code{\link{HLA_C_classification}}
 #' @source \url{https://www.ebi.ac.uk/cgi-bin/ipd/kir/retrieve_ligands.cgi?C}
 "HLA_C_class_data"
+
+#' HLA-B Bw4/Bw6-80I/T ligand classification database
+#'
+#' Classification database of HLA-B Ligands from Alleles, from Immuno Polymorphism Database
+#'
+#'
+#' @format
+#' \describe{
+#'   \item{Allele}{HLA-B allele, in format "B*01:02:01:04"}
+#'   \item{Predicted Ligand}{Predicted Ligand, "Bw6", "Bw4 - 80I" or "Bw4 - 80T"}
+#' }
+#'
+#' @seealso \code{\link{HLA_B_class_load}}, \code{\link{HLA_B_classification}}
+#' @source \url{https://www.ebi.ac.uk/cgi-bin/ipd/kir/retrieve_ligands.cgi?B}
+"HLA_B_class_data"
+
 
 #' NMDP allele codes
 #'
@@ -26,4 +42,3 @@
 #'
 #' @source \url{https://hml.nmdp.org/MacUI/}
 "NMDP"
-
--- R/pkgname.R
+++ R/pkgname.R
@@ -4,9 +4,13 @@
 #'
 #' \code{\link{HLA_C_classification}}: Classify two HLA-C alleles into a C1/C2 group
 #'
+#' \code{\link{HLA_B_classification}}: Classify two HLA-B alleles into an overall group Bw6, Bw4-80I or -80T
+#'
 #' @section Datasets:
 #'
 #' \code{\link{HLA_C_class_data}}: Reference data for C1 / C2 classification
+#'
+#' \code{\link{HLA_C_class_data}}: Reference data for Bw6, Bw4-80I and Bw4-80T classification
 #'
 #' \code{\link{NMDP}}: Reference data for expanding NMDP codes
 #'
